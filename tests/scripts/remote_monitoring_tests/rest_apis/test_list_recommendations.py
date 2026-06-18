7bfdced181f1a7bc19706135e30a85d0b0b40717
Bhaktavatsal Reddy <bhamaram@in.ibm.com>
2026-06-18 21:16:57 +0530

    more changes
    Signed-off-by: Bhaktavatsal Reddy <bhamaram@in.ibm.com>

--- tests/scripts/remote_monitoring_tests/rest_apis/test_list_recommendations.py
+++ tests/scripts/remote_monitoring_tests/rest_apis/test_list_recommendations.py
@@ -1066,7 +1066,7 @@ def test_list_recommendations_for_diff_reco_terms_with_only_latest(test_name, nu
     list_reco_json = response.json()
     assert response.status_code == SUCCESS_200_STATUS_CODE
 
-    print(json.dumps(list_reco_json))
+    #print(json.dumps(list_reco_json))
     # Validate the json against the json schema
 
     errorMsg = validate_list_reco_json(list_reco_json, reco_json_schema)
